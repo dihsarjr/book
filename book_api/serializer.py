c0dd1db4a8d9a606376f29dad9018775731a6f5a
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-06 23:03:45 +0530

    create_new_book_function_completed

--- book_api/serializer.py
+++ book_api/serializer.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from book_api.models import Book
 
 class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     number_of_page = serializers.IntegerField()
-    published_date= serializers.DateFieldJ()
-    quantity = serializers.IntegerFieldJ()
+    published_date= serializers.DateField()
+    quantity = serializers.IntegerField()
+
+    def create(self, data):
+        return Book.objects.create(**data)
 
