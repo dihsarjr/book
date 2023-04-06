1d11ed0c4788c7f8efccf0c533b0d428c8b39fb0
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-06 22:52:46 +0530

    create_book_url_added

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from book_api.views import get_book_list
+from book_api.views import get_book_list,book_create
 
 urlpatterns = [
     path('list/', get_book_list),
+    path('',book_create),
 ]
