7f0bea52f889e85af2ed2ac960507cbb87d9b12e
mohammedrashid <mohammed1rashid786@gmail.com>
2023-04-08 17:55:27 +0530

    put_and_delete_api_created

--- book_api/views.py
+++ book_api/views.py
@@ -4,13 +4,16 @@ from django.http import JsonResponse
 from book_api.serializer import BookSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
+
 
 @api_view(['GET'])
 def get_book_list(request):
     book_list = Book.objects.all()
-    serializer = BookSerializer(book_list,many = True)
+    serializer = BookSerializer(book_list, many=True)
     return Response(serializer.data)
+
 
 @api_view(['POST'])
 def book_create(request):
@@ -20,13 +23,21 @@ def book_create(request):
         return Response(serializer.data)
     else:
         return Response(serializer.errors)
-    
-@api_view(['GET','PUT','DELETE'])
-def book(request,pk):
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def book(request, pk):
+    book = Book.objects.get(pk=pk)
     if request.method == 'GET':
-        book =Book.objects.get(pk=pk)
         serializer = BookSerializer(book)
         return Response(serializer.data)
-
-
-
+    if request.method == 'PUT':
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        book.delete()
+        return Response({"success":True,"messages":"deleted"})
